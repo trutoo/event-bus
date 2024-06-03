d89b8bb8678023ea67eabcb65a7b6b1b835156ac
Erik Hughes <erik.hughes@outlook.com>
2024-06-03 14:41:54 +0200

    chore: fix package.json and remove erroneously added dist code

--- dist.compare/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { EventBus } from './event-bus';
-declare global {
-    const eventBus: EventBus;
-}
-export declare const getGlobal: () => (Window & typeof globalThis) | NodeJS.Global;
-export { EventBus };
